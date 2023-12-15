ae75875f28d5dd188dab1e7f144cb7c0b3cc1264
aisarthak <aisarthak2@gmail.com>
2023-12-16 04:02:06 +0530

    handeling schema files pull,generate,dev command used debuging
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,4 +1,15 @@
-import { PgTable, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  PgTable,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { prices, subscriptionStatus } from "../../../migrations/schema";
+import { integer } from "drizzle-orm/sqlite-core";
+import { boolean } from "drizzle-orm/mysql-core";
+import { sql } from "drizzle-orm";
 
 export const workspace = pgTable("workspace", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
@@ -48,4 +59,48 @@ export const file = pgTable("files", {
   foldersId: uuid("folders_id").references(() => folders.id, {
     onDelete: "cascade",
   }),
+});
+
+export const subscriptions = pgTable("subscriptions", {
+  id: text("id").primaryKey().notNull(),
+  userId: uuid("user_id").notNull(),
+  status: subscriptionStatus("status"),
+  metadata: jsonb("metadata"),
+  priceId: text("price_id").references(() => prices.id),
+  
+  created: timestamp("created", { withTimezone: true, mode: "string" })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodStart: timestamp("current_period_start", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodEnd: timestamp("current_period_end", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  endedAt: timestamp("ended_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  cancelAt: timestamp("cancel_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  canceledAt: timestamp("canceled_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialStart: timestamp("trial_start", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialEnd: timestamp("trial_end", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
 });
