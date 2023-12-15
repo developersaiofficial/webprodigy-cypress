51e0279e0fdcd31629b0564c3f902f2526d74f53
aisarthak <aisarthak2@gmail.com>
2023-12-15 16:58:59 +0530

    000
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import db from '../lib/supabase/db'
 
+
+console.log(db) 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,1 +1,16 @@
-export {};
+import { PgTable, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const workspace = pgTable("workspace", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  workspaceOwner: uuid("workspace_owner").notNull(),
+  title: text("title").notNull(),
+  inconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+});
