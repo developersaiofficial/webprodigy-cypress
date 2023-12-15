1bc77d9514973db2fea11213c79d35d79a6832b0
aisarthak <aisarthak2@gmail.com>
2023-12-15 15:38:49 +0530

    db>migration client
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -3,6 +3,7 @@ import postgres from "postgres";
 import * as dotenv from "dotenv";
 import * as schema from "../../../migrations/schema";
 dotenv.config({ path: "env" });
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 
 if (!process.env.DATABASE_URL) {
   console.log("🔴no DATABASE_URL");
@@ -11,5 +12,14 @@ if (!process.env.DATABASE_URL) {
 const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
 
 const db = drizzle(client, { schema });
-
+const migrateDb = async () => {
+  try {
+    console.log("🟠 Migratingn client");
+    await migrate(db, { migrationsFolder: "migration" });
+    console.log("🟢 successsuflly Migrated");
+  } catch (error) {
+    console.log("🔴 error migrating client");
+  }
+};
+migrateDb();
 export default db;
