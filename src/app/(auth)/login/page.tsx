076e042d5577908547c42273bd54d7cca07a268b
aisarthak <aisarthak2@gmail.com>
2023-12-19 04:28:28 +0530

    update page.tsx
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,16 @@
 "use client";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormSchema } from "@/lib/types";
-
+// import { Form } from "react-hook-form";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../../public/cypresslogo.svg";
+import { useRouter } from "next/navigation";
+import { Form } from "@/components/ui/form";
 
 const LoginPage = () => {
   const router = useRouter();
@@ -25,9 +30,12 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
     formData
   ) => {};
-  
 
-  return <div>LoginPage</div>;
+  return <Form>
+    <form {...form}>
+
+    </form>
+  </Form>;
 };
 
 export default LoginPage;
